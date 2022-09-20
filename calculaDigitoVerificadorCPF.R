2a53f04f8f3a09af549f9ab1c34f6d1a4d742906
mano-k9 <93453179+mano-k9@users.noreply.github.com>
2022-09-20 14:03:17 -0300

    Create calculaDigitoVerificadorCPF.R

---
+++ calculaDigitoVerificadorCPF.R
@@ -0,0 +1,20 @@
+digitoCpf <- function(strCpf9){
+  numbers_only <- function(x) !grepl("\\D", x)
+  if ((nchar(strCpf9) != 9) | (!numbers_only(strCpf9))){return("--")}
+  calcDig <- function(strCpf){
+    multi <- nchar(strCpf) + 1
+    somaDigitos <- 0
+    for  (digitoStr in strsplit(strCpf, "")[[1]]){
+      digito <- as.integer(strtoi(digitoStr) * multi)
+      somaDigitos <- somaDigitos + digito
+      multi <- multi - 1
+    }
+    calcMod11 <- 11 - (somaDigitos %% 11)
+    if (calcMod11 > 9){calcMod11 <- 0}
+    digitoVerificador = toString(calcMod11)
+    return(digitoVerificador)
+  }
+  digitoVerificador1 <- calcDig(strCpf9)
+  digitoVerificador2 <- calcDig(paste(strCpf9,digitoVerificador1,sep=""))
+  return(paste(digitoVerificador1,digitoVerificador2,sep=""))
+}
